882dbbf1d4d792359fb9cf4af45ce6c0479887d0
Tassilo Horn <tassilo@member.fsf.org>
2012-03-13 21:44:14 +0100

    init

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject concat "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.4.0-beta4"]])
---
+++ src/concat/core.clj
@@ -0,0 +1,81 @@
+(ns concat.core)
+
+;;# Stack & Words
+
+(def ^{:dynamic true :private true}
+  *stack* nil)
+(def ^:private
+  +words+ {})
+
+;;# Utilities
+
+(defn push! [& args]
+  (swap! *stack* into args))
+
+;;# Registering words
+
+(defn register-word [name argcount definition]
+  (alter-var-root #'+words+ assoc name [argcount definition]))
+
+;;# The core words
+
+(register-word :dup         1 #(push! %1 %1))
+(register-word :swap        2 #(push! %2 %1))
+(register-word :print-top   0 #(println (first @*stack*)))
+(register-word :print-stack 0 #(println @*stack*))
+
+;;# Defining words from clojure functions
+
+(defmacro register-words-for-clj-fns [word argcount fn & more]
+  `(do
+     (register-word ~word ~argcount
+                    (fn [& args#]
+                      (push! (apply ~fn args#))))
+     ~(when (seq more)
+        `(register-words-for-clj-fns
+          ~(nth more 0) ~(nth more 1) ~(nth more 2)
+          ~@(drop 3 more)))))
+
+(register-words-for-clj-fns
+ :+       2   +
+ :-       2   -
+ :*       2   *
+ :div     2   /
+ :filter  2   filter
+ :reduce  3   reduce
+ :reduce1 3   reduce
+ :map1    2   map
+ :map2    3   map
+ :map3    4   map)
+
+;;# The runtime
+
+(defn- extract-args [n]
+  (let [[r s] ((juxt take drop) n @*stack*)]
+    (swap! *stack* (constantly s))
+    (reverse r)))
+
+(defn- concat-apply [a]
+  (if-let [spec (+words+ a)]
+    (apply (spec 1) (extract-args (spec 0)))
+    (push! a)))
+
+(defn- run-concat-1
+  ([args & words]
+     (run-concat-1 args)
+     (run-concat-1 words))
+  ([args]
+     (doseq [a args]
+       (concat-apply a))))
+
+(defn run-concat [& args]
+  (binding [*stack* (atom (list))]
+    (run-concat-1 args)))
+
+;;# Defining words in the concat language itself
+
+(register-word :define-word 3
+               (fn [name argcount definition]
+                 (register-word name argcount
+                                (fn [& args]
+                                  (apply run-concat-1 args definition)))))
